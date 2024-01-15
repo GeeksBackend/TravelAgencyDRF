9f10775af75cda5606b75f272d3230a9b4bed875
Toktorov <ktoktorov2005@gmail.com>
2024-01-15 09:26:06 +0600

    add crud for tours

--- apps/tours/urls.py
+++ apps/tours/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path 
 
-from apps.tours.views import TourAPI, TourAPICreate
+from apps.tours.views import TourAPI, TourAPICreate, TourAPIRetrieve, TourAPIUpdate, TourAPIDestroy
 
 
 urlpatterns = [
     path('', TourAPI.as_view(), name="api_tours"),
-    path('create/', TourAPICreate.as_view(), name="api_tours_create")
+    path('create/', TourAPICreate.as_view(), name="api_tours_create"),
+    path('<int:pk>/', TourAPIRetrieve.as_view(), name="api_tours_retrieve"),
+    path('update/<int:pk>/', TourAPIUpdate.as_view(), name="api_tours_update"),
+    path('destroy/<int:pk>/', TourAPIDestroy.as_view(), name="api_tours_destroy")
 ]
--- apps/tours/views.py
+++ apps/tours/views.py
@@ -1,13 +1,25 @@
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from apps.tours.models import Tour
 from apps.tours.serializers import TourSerializers
 
 # Create your views here.
 class TourAPI(ListAPIView):
-    queryset = Tour.objects.all()
+    queryset = Tour.objects.all() #SELECT * FROM tours_tour; (Django ORM)
     serializer_class = TourSerializers
 
 class TourAPICreate(CreateAPIView):
+    queryset = Tour.objects.all()
+    serializer_class = TourSerializers
+
+class TourAPIRetrieve(RetrieveAPIView):
+    queryset = Tour.objects.all()
+    serializer_class = TourSerializers
+
+class TourAPIUpdate(UpdateAPIView):
+    queryset = Tour.objects.all()
+    serializer_class = TourSerializers
+
+class TourAPIDestroy(DestroyAPIView):
     queryset = Tour.objects.all()
     serializer_class = TourSerializers
